f9bf9350f9c554b1fdfbe2635ee0b024b67fa24e
paulnguyen <pn2yr@virginia.edu>
2016-10-26 18:11:59 -0400

    Removed get_csd() and get_freq_amplitudes() functions

--- FilterNetwork.py
+++ FilterNetwork.py
@@ -8,29 +8,6 @@ from util import *
 FILTER_INDEX=0
 SIGNAL_COMBINER_INDEX=1
 
-def get_freq_amplitudes(input_signal, sampling_freq):
-    # Returns a dict representing the power spectrum. Keys are frequencies. Values are amplitudes. 
-    num_samples = len(input_signal)
-    freq_res = float(sampling_freq) / num_samples
-    return dict(zip([freq_res*i for i in xrange(num_samples)], ((2.0*numpy.absolute(numpy.fft.fft(input_signal))[:int(math.floor(sampling_freq/(freq_res*2.0)))+1])/float(num_samples))))
-
-def get_csd(signal_a, signal_b, sample_rate):
-    samples_per_segment = sample_rate#/10 # sample_rate times x makes each segment x seconds
-    samples_per_segment = samples_per_segment if samples_per_segment%2==0 else samples_per_segment+1 # to make sure it's odd as the tukey window needs an odd number of samples
-    ans = scipy.signal.csd(
-        signal_a, 
-        signal_b, 
-        sample_rate, 
-        window=scipy.signal.get_window(('tukey',0.5),samples_per_segment), #Using a tukey window so that it only diminishes volume near the ends of the segment
-        nperseg=samples_per_segment,
-        noverlap=None, # we set this to None so that it defauls to nperseg/2
-        nfft=None, # we set this to None so that it defaults to using nperseg
-        detrend='constant', # to remove DC offset
-        return_onesided=True, # to remove aliasing above nyquist limit
-        scaling='density', # doesn't matter for our purposes since we only care about these values relative to each other
-        axis=-1)
-    return ans # might have complex values, but they should all be zero since all of our input signals are real-valued
-
 def get_signal_similarity(signal_a, signal_b, sample_rate):
     samples_per_segment = sample_rate#/10 # sample_rate times x makes each segment x seconds
     samples_per_segment = samples_per_segment if samples_per_segment%2==0 else samples_per_segment+1 # to make sure it's odd as the tukey window needs an odd number of samples
